38938577120ae8abf5680c26ef6bf2b3eed36fb4
FominNickolai <giraffe.in.ua@gmail.com>
2017-10-23 21:02:21 +0300

    Update RangeSeekSlider.swift

--- Sources/RangeSeekSlider.swift
+++ Sources/RangeSeekSlider.swift
@@ -8,20 +8,6 @@
 
 import UIKit
 
-open class VerticallyCenteredTextLayer: CATextLayer {
-    override open func draw(in ctx: CGContext) {
-        let fontSize = self.fontSize
-        let height = self.bounds.size.height
-        let deltaY = (height-fontSize)/2 - fontSize/10
-        
-        ctx.saveGState()
-        ctx.translateBy(x: 0.0, y: deltaY)
-        super.draw(in: ctx)
-        ctx.restoreGState()
-        
-    }
-}
-
 @IBDesignable open class RangeSeekSlider: UIControl {
 
     // MARK: - initializers
